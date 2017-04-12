4dc2f03315dfb3f8ae88498c89b79b0d52145d0a
NicV2 <nicv2code@gmail.com>
2017-04-12 08:26:36 -0700

    Working on resetting the board.

--- sketchpad.js
+++ sketchpad.js
@@ -28,6 +28,25 @@ $(".sketchpad").hover(function()
 //Click on "clear grid" button, user prompt for squares per side
 $("#button").click(function(){
   var squares=prompt("Time for a new board! How many squares per side this time?");
+       $(".container").empty();
+       console.log(squares);
+  for (var k=0; k<squares; k++)
+  {
+  	for (var l=0; l<squares; l++)
+  	{
+  		var newSquare=$("<div>Hello</div>");
+  		$("newSquare").width("48em/squares");
+  		$("newSquare").height("48em/squares");
+  		$("newSquare").css("background-color", "#7d9ec0");
+  		$("newSquare").css("border-style", "solid");
+		$("newSquare").css("border-width", ".25px");
+		$("newSquare").css("display", "inline-block");
+		$("newSquare").css("float", "left");
+		$("newSquare").appendTo(".container");
+
+  		console.log("added");
+  	}
+  }
 });
 
 
